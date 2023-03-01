2c8fcc5589fedcba40f567190778c63cb0d57a90
unknown <umeshnidoni1997@gmail.com>
2023-03-01 11:47:12 +0530

    New changes made

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -10,7 +10,8 @@ function App() {
   }
   return <>
  <input type="text" onChange={(e)=>setNAME(e.target.value)}/><br/>
- <input type="text" onChange={(e)=>setNAME(e.target.value)} placeholder="PASSWORD"/><br/>
+ <input type="text" onChange={(e)=>setNAME(e.target.value)} placeholder="Email"/><br/>
+ <input type="text" onChange={(e)=>setNAME(e.target.value)} placeholder="Password"/><br/>
  <button type="submit" onClick={METHOD}>BUTTON</button>
   </>
 }
