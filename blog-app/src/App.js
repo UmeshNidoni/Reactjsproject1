9e38f045627cb55393c9b8294780ff399e325123
UmeshNidoni <60518123+UmeshNidoni@users.noreply.github.com>
2023-03-01 10:57:33 +0530

    Add files via upload
    new changes

---
+++ blog-app/src/App.js
@@ -0,0 +1,17 @@
+import {React} from "react";
+import axios from "axios";
+import { useState } from "react";
+function App() {
+  const [NAME,setNAME]=useState("");
+  console.log(NAME);
+
+  const METHOD=()=>{
+    axios.post(`http://localhost:8282/api/apicall?NAME=${NAME}`)
+  }
+  return <>
+ <input type="text" onChange={(e)=>setNAME(e.target.value)}/><br/>
+ <button type="submit" onClick={METHOD}>BUTTON</button>
+  </>
+}
+
+export default App;
